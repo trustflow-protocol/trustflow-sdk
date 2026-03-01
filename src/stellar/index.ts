47865d9201c892b438c199e266b24e9508999a26
OWK50GA <okoriewifrid.20201206203@futo.edu.ng>
2026-03-01 11:00:00 +0100

    feat(sdk/stellar): barrel export Stellar helpers

---
+++ src/stellar/index.ts
@@ -0,0 +1,5 @@
+export { loadAccountInfo, accountExists } from './account';
+export { buildBaseTransaction, setTransactionTimeout } from './transaction';
+export { signWithFreighter } from './signing';
+export { submitTransaction } from './horizon';
+export type { AccountInfo, SignedTransaction } from './account';
