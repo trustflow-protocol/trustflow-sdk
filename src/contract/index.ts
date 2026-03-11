626358f64dbb358ad48f3f931b31ffaa56d1d23a
Mercy Duru <m-duru@leadway.com>
2026-03-11 09:00:00 +0100

    feat(sdk/contract): barrel export contract helpers

---
+++ src/contract/index.ts
@@ -0,0 +1,5 @@
+export { invokeContract } from './invoke';
+export { readContractState } from './read';
+export { simulateContractCall } from './simulate';
+export { buildCreateEscrowArgs, buildReleaseArgs, buildDisputeArgs } from './build';
+export type { SimulationResult } from './simulate';
