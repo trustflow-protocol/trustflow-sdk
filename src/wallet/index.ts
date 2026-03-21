4bcf7e7ec80c4da7436c1aa79099836de83405ab
Kaylahray <chiomachristopher91@gmail.com>
2026-03-21 10:00:00 +0100

    feat(sdk/wallet): barrel export wallet module

---
+++ src/wallet/index.ts
@@ -0,0 +1,4 @@
+export { getFreighter, isFreighterInstalled } from './freighter';
+export { getAlbedo } from './albedo';
+export { connectWallet, disconnectWallet } from './connect';
+export type { WalletType, WalletConnection, WalletAdapter } from './types';
